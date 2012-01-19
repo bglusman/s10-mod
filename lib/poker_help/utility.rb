e25fe6856471048b2f2e9e04c53382231ed41d69
bglusman <brian@enjyn.com>
2012-01-19 01:40:54 -0500

    cleaning up utility

--- lib/poker_help/utility.rb
+++ lib/poker_help/utility.rb
@@ -5,32 +5,31 @@ module PokerHelp
         :royal_flush?, :full_house?, :three_of_a_kind?, :two_pair?])
 
       candidates = {}
-      hits = 0
-      #cards_remaining.times do
-        deck.each do |card|
-          deck_copy = Marshal.load( Marshal.dump(deck) )
-          hand_copy = Marshal.load( Marshal.dump(hand) )
-          # binding.pry if hand_copy.to_a.include?(card)
-          hand_copy << card
-          deck_copy.burn(card)
-          candidates[deck_copy] = hand_copy
-        end                                         #only count outs once
-        hits = outs(candidates, targets) if candidates.size == deck.size
-      #end
+
+      deck.each do |card|
+        deck_copy = Marshal.load( Marshal.dump(deck) )
+        hand_copy = Marshal.load( Marshal.dump(hand) )
+        hand_copy << card
+        deck_copy.burn(card)
+        candidates[deck_copy] = hand_copy
+      end
+
+      hits = outs(candidates, targets)
+
       size = candidates.size.to_f
       probabilities = []
       0.upto(cards_remaining-1) do |chance_num|
-        probabilities << (((size - chance_num) - outs(candidates, targets).size)/size)
+        probabilities << (((size - chance_num) - hits)/(size - chance_num))
       end
 
       total = 1 - probabilities.reduce(1) {|accum, x| accum * x}
 
-      [hits.count, total, probabilities.map{|p| 1-p}]
+      [hits, total, probabilities.map{|p| 1-p}]
     end
 
     def outs(candidates, targets)
       candidates.select do |deck, hand|
-          targets.reduce(false) {|bool, target| bool || hand.send(target)}
+          targets.reduce(false) {|bool, target| bool || hand.send(target)}.size
         end
     end
   end
